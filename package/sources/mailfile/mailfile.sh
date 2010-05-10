922a73b83135bbe85bb4369789976f10055779b2
Jason Kenah <jason@ii-tec.com>
2010-05-10 07:53:33 +0000

    New Fax2Email module Tooltips now use global language variable Added ability to route incomming SIP calls by SIP header (for multiple SIP registrations from one provider) SMTP settings now in "System Setup" A few resize/alignment fixes
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@109 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/sources/mailfile/mailfile.sh
+++ package/sources/mailfile/mailfile.sh
@@ -3,22 +3,48 @@
 #Syntax:
 #	 mailfile file_name somboady@domain.com 
 
+random_number=`date '+%s'`
+tmpfile='/tmp/email'$random_number
+configfile='/etc/asterisk/fax2email.conf'
 
-#Template of the e-mail using MIME protocol
-cp /bin/mailfile_header_template /tmp/email
+subject=`sed '/^\#/d' $configfile | grep 'emailsubject' | tail -n 1 | sed 's/^.*=//;s/^[[:space:]]*//;s/[[:space:]]*$//'`
+sender=`sed '/^\#/d' $configfile | grep 'serveremail' | tail -n 1 | sed 's/^.*=//;s/^[[:space:]]*//;s/[[:space:]]*$//'`
+body=`sed '/^\#/d' $configfile | grep 'emailbody' | tail -n 1 | sed 's/^.*=//;s/^[[:space:]]*//;s/[[:space:]]*$//'`
 
-#Include the file name in the template
-sed -i "s/name_of_the_file/$1/g" /tmp/email 
+#email header
+echo 'Subject: '$subject > $tmpfile
+echo 'From: '$sender >> $tmpfile
+echo 'Content-Type: multipart/mixed;' >> $tmpfile
+echo ' boundary="------------060502030501040302050009"' >> $tmpfile
+echo ' ' >> $tmpfile
+echo 'This is a multi-part message in MIME format.' >> $tmpfile
+echo '--------------060502030501040302050009' >> $tmpfile
+echo 'Content-Type: text/plain; charset=ISO-8859-1; format=flowed' >> $tmpfile
+echo 'Content-Transfer-Encoding: 7bit' >> $tmpfile
 
-#Encode the file
-uuencode -m $1 $1 | sed '1d$d' >> /tmp/email
+#email body and replace /n with new line
+echo '' >> $tmpfile
+echo $body >> $tmpfile
+echo '' >> $tmpfile
 
-#Add footer
-cat /bin/mailfile_footer_template >> /tmp/email
+#tiff file
+echo '--------------060502030501040302050009' >> $tmpfile
+echo 'Content-Type: image/tiff;' >> $tmpfile
+echo ' name="'$1'"' >> $tmpfile
+echo 'Content-Transfer-Encoding: base64' >> $tmpfile
+echo 'Content-Disposition: attachment;' >> $tmpfile
+echo ' filename="'$1'"' >> $tmpfile
+
+#encode the file
+uuencode -m $1 $1 | sed '1d$d' >> $tmpfile
+
+#email footer
+echo '' >> $tmpfile
+echo '--------------060502030501040302050009--' >> $tmpfile
 
 #Finally send the e-mail using ssmtp
-cat /tmp/email | ssmtp $2
+cat $tmpfile | ssmtp $2
 
 #clean the tmp files
-rm /tmp/email
+rm $tmpfile
 
