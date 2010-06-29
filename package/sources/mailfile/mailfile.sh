02d47671d3457d5fba29cb653c1aecd19676c890
Jason Kenah <jason@ii-tec.com>
2010-06-29 04:58:25 +0000

    Fax2Email filetype (pdf/tiff) added dialoptions to ring groups
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@262 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/sources/mailfile/mailfile.sh
+++ package/sources/mailfile/mailfile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #This script send a file as e-mailattachment using ssmtp mail transfer agent
 #Syntax:
-#	 mailfile file_name somboady@domain.com 
+#	 mailfile file_name somboady@domain.com filetype(tiff/pdf)
 
 random_number=`date '+%s'`
 tmpfile='/tmp/email'$random_number
@@ -24,18 +24,29 @@ echo 'Content-Transfer-Encoding: 7bit' >> $tmpfile
 
 #email body and replace /n with new line
 echo '' >> $tmpfile
-echo $body | sed 's/\n/\n/g' >> $tmpfile
+echo $body | sed 's/\/n/\n/g' >> $tmpfile
 echo '' >> $tmpfile
 
 #tiff file
 echo '--------------060502030501040302050009' >> $tmpfile
-echo 'Content-Type: image/tiff;' >> $tmpfile
-echo ' name="'$1'"' >> $tmpfile
-echo 'Content-Transfer-Encoding: base64' >> $tmpfile
-echo 'Content-Disposition: attachment;' >> $tmpfile
-echo ' filename="'$1'"' >> $tmpfile
+if [ -f "/bin/tiff2pdf" ]; then
+  echo 'Content-Type: image/'$3';' >> $tmpfile
+  echo ' name="'$1'.'$3'"' >> $tmpfile
+  echo 'Content-Transfer-Encoding: base64' >> $tmpfile
+  echo 'Content-Disposition: attachment;' >> $tmpfile
+  echo ' filename="'$1'.'$3'"' >> $tmpfile
+else
+  echo 'Content-Type: image/tiff;' >> $tmpfile
+  echo ' name="'$1'.tiff"' >> $tmpfile
+  echo 'Content-Transfer-Encoding: base64' >> $tmpfile
+  echo 'Content-Disposition: attachment;' >> $tmpfile
+  echo ' filename="'$1'.tiff"' >> $tmpfile
+fi
 
 #encode the file
+if [ $3 = "pdf" ] && [ -f "/bin/tiff2pdf" ]; then
+  tiff2pdf $1 -c "SwitchFin PBX" -o $1
+fi
 uuencode -m $1 $1 | sed '$d' >> $tmpfile
 
 #email footer
@@ -47,4 +58,5 @@ cat $tmpfile | ssmtp $2
 
 #clean the tmp files
 rm $tmpfile
+rm $1
 
