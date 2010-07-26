16b9d3f20f70fb1c6c422979d826aba4873cbbbd
Dimitar Penev <dpn@switchvoice.com>
2010-07-26 22:43:00 +0000

    improve it
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@300 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/sources/mailfile/mailfile.sh
+++ package/sources/mailfile/mailfile.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 #This script send a file as e-mailattachment using ssmtp mail transfer agent
+#Note that the input tiff file will be deleted! 
+#
 #Syntax:
-#	 mailfile file_name somboady@domain.com filetype(tiff/pdf)
+#	mailfile file_name somboady@domain.com [filetype]
+#
+#Arguments:	
+#	file_name 		- Input tiff file name
+#	somboady@domain.com 	- E-mail address
+#	filetype		- Optional argument can be tiff or pdf
+#				  If ommited tiff is assumed. 
+#				  pdf type is used only if tiff2pdf tool is installed
+#	
+# 	  			
 
 random_number=`date '+%s'`
 tmpfile='/tmp/email'$random_number
@@ -29,25 +40,23 @@ echo '' >> $tmpfile
 
 #tiff file
 echo '--------------060502030501040302050009' >> $tmpfile
-if [ -f "/bin/tiff2pdf" ]; then
+if [ "$3" == "pdf" ] && [ -f "/bin/tiff2pdf" ]; then
   echo 'Content-Type: image/'$3';' >> $tmpfile
-  echo ' name="'$1'.'$3'"' >> $tmpfile
+  echo ' name="'${1%.*}'.'pdf'"' >> $tmpfile
   echo 'Content-Transfer-Encoding: base64' >> $tmpfile
   echo 'Content-Disposition: attachment;' >> $tmpfile
-  echo ' filename="'$1'.'$3'"' >> $tmpfile
+  echo ' filename="'${1%.*}'.'pdf'"' >> $tmpfile
+  tiff2pdf $1 -c "SwitchFin PBX" -o ${1%.*}.pdf
+  uuencode -m ${1%.*}.pdf ${1%.*}.pdf | sed '1d$d' >> $tmpfile
+  rm ${1%.*}.pdf
 else
   echo 'Content-Type: image/tiff;' >> $tmpfile
-  echo ' name="'$1'.tiff"' >> $tmpfile
+  echo ' name="'$1'"' >> $tmpfile
   echo 'Content-Transfer-Encoding: base64' >> $tmpfile
   echo 'Content-Disposition: attachment;' >> $tmpfile
-  echo ' filename="'$1'.tiff"' >> $tmpfile
+  echo ' filename="'$1'"' >> $tmpfile
+  uuencode -m $1 $1 | sed '1d$d' >> $tmpfile 
 fi
-
-#encode the file
-if [ $3 = "pdf" ] && [ -f "/bin/tiff2pdf" ]; then
-  tiff2pdf $1 -c "SwitchFin PBX" -o $1
-fi
-uuencode -m $1 $1 | sed '$d' >> $tmpfile
 
 #email footer
 echo '' >> $tmpfile
@@ -56,6 +65,6 @@ echo '--------------060502030501040302050009--' >> $tmpfile
 #Finally send the e-mail using ssmtp
 cat $tmpfile | ssmtp $2
 
-#clean the tmp files
+#clean the tiff and tmp file
 rm $tmpfile
 rm $1
