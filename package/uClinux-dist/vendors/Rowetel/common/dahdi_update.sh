67fb3439181390ad32d25d1e69b80fdbffb8cb98
Dimitar Penev <dpn@switchvoice.com>
2010-11-18 00:12:31 +0000

    dahdi_update.sh made consistent with the way GUI save lec settings
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@441 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/uClinux-dist/vendors/Rowetel/common/dahdi_update.sh
+++ package/uClinux-dist/vendors/Rowetel/common/dahdi_update.sh
@@ -1,26 +1,26 @@
 #!/bin/hush
 
-fxs=`dahdi_scan | grep FXS | sed 's/port=\([1-8]\),FXS/\1 /' | sed 's/\([1-8]\)/\1,/g' | tr -d "\n" | sed 's/,.$//'`
-fxo=`dahdi_scan | grep FXO | sed 's/port=\([1-8]\),FXO/\1 /' | sed 's/\([1-8]\)/\1,/g' | tr -d "\n" | sed 's/,.$//'`
+fxs=`dahdi_scan | grep FXS | sed 's/port=\([1-8]\),FXS/\1 /' | sed 's/\([1-8]\)/\1,/g' | tr -d "\n" | sed 's/,.$//' | tr -d " "`
+fxo=`dahdi_scan | grep FXO | sed 's/port=\([1-8]\),FXO/\1 /' | sed 's/\([1-8]\)/\1,/g' | tr -d "\n" | sed 's/,.$//' | tr -d " "`
 
 echo loadzone = us > /etc/dahdi/system.conf
 echo defaultzone = us >> /etc/dahdi/system.conf
-
-if [ ! -z "$fxs" ]; then
-	echo fxoks=$fxs >> /etc/dahdi/system.conf
-	channels="$fxs"
-fi
-
+                                               
+if [ ! -z "$fxs" ]; then                       
+        echo fxoks=$fxs >> /etc/dahdi/system.conf
+        channels="$fxs"                          
+fi                                               
+                                                 
 if [ ! -z "$fxo" ]; then
-	echo fxsks=$fxo >> /etc/dahdi/system.conf
-	if [ ! -z "$channels" ]; then
-		channels="$fxs, $fxo"
-	else
-		channels="$fxo"
-		
-	fi
-fi
-
+        echo fxsks=$fxo >> /etc/dahdi/system.conf
+        if [ ! -z "$channels" ]; then            
+                channels="$fxs,$fxo"             
+        else                                     
+                channels="$fxo"      
+                                    
+        fi                     
+fi                             
+          
 if [ ! -z "$channels" ]; then
-	echo echocanceller=oslec,$channels >> /etc/dahdi/system.conf
-fi
+        echo "echocanceller = oslec",$channels >> /etc/dahdi/system.conf
+fi 
