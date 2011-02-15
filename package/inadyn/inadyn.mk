fb592a5f35ea41928b6e0ee860e5496b84a7925c
Dimitar Penev <dpn@switchvoice.com>
2011-02-15 10:57:45 +0000

    Inadyn updated, Log in the GUI now posible
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@491 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/inadyn/inadyn.mk
+++ package/inadyn/inadyn.mk
@@ -4,30 +4,29 @@
 #
 # Copyright @ 2010 SwitchFin <dpn@switchfin.org>
 #########################################################
-
-INADYN_SITE=http://www.inatech.eu/inadyn
-INADYN_VERSION=1.96.2
-INADYN_SOURCE=inadyn.v$(INADYN_VERSION).zip
+INADYN_SITE=ftp://ftp.vmlinux.org/pub/People/jocke/inadyn/
+INADYN_VERSION=1.97.4
+INADYN_SOURCE=inadyn-$(INADYN_VERSION).tar.bz2
 INADYN_CROSS_COMPILE_PATCH=inadyn.patch
 INADYN_DIR_BASENAME=inadyn
-INADYN_DIR=$(BUILD_DIR)/$(INADYN_DIR_BASENAME)
+INADYN_DIR=$(BUILD_DIR)/$(INADYN_DIR_BASENAME)-$(INADYN_VERSION)
 
 
 $(DL_DIR)/$(INADYN_SOURCE):
 	$(WGET) -P $(DL_DIR) $(INADYN_SITE)/$(INADYN_SOURCE)
 
 $(INADYN_DIR)/.unpacked: $(DL_DIR)/$(INADYN_SOURCE)
-	unzip $(DL_DIR)/$(INADYN_SOURCE) -d $(BUILD_DIR)
+	tar -xf $(DL_DIR)/$(INADYN_SOURCE) -C $(BUILD_DIR)
 	touch $(INADYN_DIR)/.unpacked
 
 $(INADYN_DIR)/.configured: $(INADYN_DIR)/.unpacked
-	patch -p1 -d $(INADYN_DIR) < package/inadyn/$(INADYN_CROSS_COMPILE_PATCH)
+	patch -p0 -d $(INADYN_DIR) < package/inadyn/$(INADYN_CROSS_COMPILE_PATCH)
 	touch $(INADYN_DIR)/.configured
 
 inadyn: $(INADYN_DIR)/.configured
 	make -C $(INADYN_DIR) CC=bfin-linux-uclibc-gcc STRIP=bfin-linux-uclibc-strip LD=bfin-linux-uclibc-gcc os=linux
 	mkdir -p $(TARGET_DIR)/bin/
-	cp -f $(INADYN_DIR)/bin/linux/inadyn $(TARGET_DIR)/bin/
+	cp -f $(INADYN_DIR)/src/inadyn $(TARGET_DIR)/bin/
 	$(TARGET_STRIP) $(TARGET_DIR)/bin/inadyn	
 
 
