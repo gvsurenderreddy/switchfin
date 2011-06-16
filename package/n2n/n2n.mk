49a076afa9fbde9cf3fda0e18ab7d6509a7da9b4
James Le Cuirot <chewi@aura-online.co.uk>
2011-06-16 21:44:25 +0000

    Add n2n v2, with a menuconfig choice to choose between this and v1. Makefile now builds it faster and without the need for a patch.
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@559 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/n2n/n2n.mk
+++ package/n2n/n2n.mk
@@ -4,41 +4,43 @@
 #              LI YUQIAN <yu@edgepbx.cn>           #
 ####################################################
 
-N2N_DIR=$(DL_DIR)/n2n
-N2N_SOURCE=n2n
+ifeq ($(strip $(SF_N2N_V2)),y)
+	N2N_VERSION=2
+else
+	N2N_VERSION=1
+endif
+
 N2N_SITE=https://svn.ntop.org/svn/ntop/trunk/n2n
-N2N_VERSION=3869
-N2N_WORKING=$(BUILD_DIR)/$(N2N_SOURCE)
+N2N_REVISION=4661
+N2N_DIR=$(DL_DIR)/n2n
+N2N_SUBDIR=n2n_v$(N2N_VERSION)
+N2N_WORKING=$(BUILD_DIR)/$(N2N_SUBDIR)
 
 
-$(DL_DIR)/$(N2N_SOURCE):
-	$(SVN) $(N2N_SITE) --revision $(N2N_VERSION) $(DL_DIR)/$(N2N_SOURCE)
-	-find $(DL_DIR)/$(N2N_SOURCE) -type d -name .svn | xargs rm -rf
+$(N2N_DIR):
+	$(SVN) $(N2N_SITE) --revision $(N2N_REVISION) $(N2N_DIR)
 
-$(N2N_WORKING)/.unpacked: $(DL_DIR)/$(N2N_SOURCE)
-	cp -Rf $(DL_DIR)/$(N2N_SOURCE) $(N2N_WORKING)
+$(N2N_WORKING)/.unpacked: $(N2N_DIR)
+	rm -rf $(N2N_WORKING)
+	cp -R $(N2N_DIR)/$(N2N_SUBDIR) $(N2N_WORKING)
 	touch $(N2N_WORKING)/.unpacked
 
 $(N2N_WORKING)/.configured: $(N2N_WORKING)/.unpacked
-	$(PATCH_KERNEL) $(N2N_WORKING) package/n2n n2n.patch
+	-find $(N2N_WORKING) -type d -name .svn | xargs rm -rf
 	touch $(N2N_WORKING)/.configured
 
 n2n: $(N2N_WORKING)/.configured
-	make -C $(N2N_WORKING)
-	cp -f $(N2N_WORKING)/edge $(TARGET_DIR)/usr/sbin/ 
-	cp -f $(N2N_WORKING)/supernode $(TARGET_DIR)/usr/bin/ 
+	CFLAGS="-O3 $(TARGET_CFLAGS) -I$(STAGING_INC) -L$(STAGING_LIB)" make -C $(N2N_WORKING) CC=$(TARGET_CC) DEBUG= edge supernode
+	cp -f $(N2N_WORKING)/{edge,supernode} $(TARGET_DIR)/usr/sbin/
 
-n2n-clean:
-	rm -rf $(N2N_WORKING)
-n2n-dirclean:
-	rm -rf $(N2N_WORKING)
+n2n-clean n2n-dirclean:
+	rm -rf $(BUILD_DIR)/n2n_v[12]
 
 ifeq ($(strip $(SF_PACKAGE_N2N)),y)
 n2n_: n2n
 else
 n2n_:
-	rm -f $(TARGET_DIR)/usr/sbin/edge
-	rm -f $(TARGET_DIR)/usr/bin/supernode
+	rm -f $(TARGET_DIR)/usr/sbin/{edge,supernode}
 endif
 
 ################################################
