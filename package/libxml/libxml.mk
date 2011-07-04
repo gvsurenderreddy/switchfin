492b81c31d1946d74dabed9558bedea02eff07b4
James Le Cuirot <chewi@aura-online.co.uk>
2011-07-04 15:30:07 +0000

    Clean up libxml package. Important variables like CC should never be exported here. This and most of the other variables didn't even need to be defined anyway. Trust your build system! Admittedly this hasn't been tested on a box but the old version wasn't working anyway due to the 2.6.31 tarball not existing anymore.
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@586 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/libxml/libxml.mk
+++ package/libxml/libxml.mk
@@ -17,13 +17,11 @@
 #####################################################
 
 LIBXML_SITE=ftp://xmlsoft.org/libxml2
-LIBXML_VERSION=2.6.31
+LIBXML_VERSION=2.7.8
 LIBXML_SOURCE=libxml2-sources-$(LIBXML_VERSION).tar.gz
 LIBXML_UNZIP=zcat
-LIBXML_CONFIGURE_OPTS=--host=bfin-linux-uclibc --build=i686-linux --without-python --prefix=$(STAGING_DIR)
+LIBXML_CONFIGURE_OPTS=--host=$(CROSS_COMPILE) --without-python
 LIBXML_DIR=$(BUILD_DIR)/libxml2-$(LIBXML_VERSION)
-
-export CC=bfin-linux-uclibc-gcc
 
 $(DL_DIR)/$(LIBXML_SOURCE):
 	$(WGET) -P $(DL_DIR) $(LIBXML_SITE)/$(LIBXML_SOURCE)
@@ -34,23 +32,14 @@ $(LIBXML_DIR)/.unpacked: $(DL_DIR)/$(LIBXML_SOURCE)
 	$(LIBXML_UNZIP) $(DL_DIR)/$(LIBXML_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	touch $(LIBXML_DIR)/.unpacked
 
-
 $(LIBXML_DIR)/.configured: $(LIBXML_DIR)/.unpacked
-	export CC=bfin-linux-uclibc-gcc
 	cd $(LIBXML_DIR); ./configure $(LIBXML_CONFIGURE_OPTS)
 	touch $(LIBXML_DIR)/.configured
 
 libxml: $(LIBXML_DIR)/.configured
-	$(MAKE) -C "$(LIBXML_DIR)" ARCH="bfin-linux-uclibc"  \
-                LIBDIR="usr/lib"\
-                CROSS="$(TARGET_CROSS)" \
-                CFLAGS="$(LIBXML_CFLAGS)" \
-                LDFLAGS="-L$(STAGING_LIB) -mfdpic -lpthread -ldl" \
-		install
-	cp -v $(LIBXML_DIR)/.libs/libxml2.so $(TARGET_DIR)/lib/libxml2.so
-	cd $(TARGET_DIR)/lib; ln -sf libxml2.so libxml2.so.2
-
-
+	$(MAKE) -C "$(LIBXML_DIR)" libxml2.la
+	$(TARGET_STRIP) --strip-unneeded -o $(TARGET_DIR)/lib/libxml2.so $(LIBXML_DIR)/.libs/libxml2.so
+	ln -sf libxml2.so $(TARGET_DIR)/lib/libxml2.so.2
 
 libxml-clean:
 	rm -rf $(LIBXML_DIR)/.configured
