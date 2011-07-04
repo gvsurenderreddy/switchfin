94bd3f788ba74ce93bf25b9b9f264f37361b22b4
James Le Cuirot <chewi@aura-online.co.uk>
2011-07-04 16:46:38 +0000

    Fix and bump rsync to 2.6.8. Many thanks to Jamie Lokier for the additional patches. Functionality has been heavily limited due to a reliance on forking.
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@588 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/rsync/rsync.mk
+++ package/rsync/rsync.mk
@@ -4,12 +4,12 @@
 #
 #########################################################
 
-RSYNC_SITE=http://downloads.openwrt.org/sources
-RSYNC_VERSION=2.6.5
-RSYNC_SOURCE=rsync-2.6.5.tar.gz
+RSYNC_SITE=http://rsync.samba.org/ftp/rsync
+RSYNC_VERSION=2.6.8
+RSYNC_SOURCE=rsync-2.6.8.tar.gz
 RSYNC_UNZIP=zcat
 RSYNC_DIR=$(BUILD_DIR)/rsync-$(RSYNC_VERSION)
-RSYNC_CONFIGURE_OPTS=--host=bfin-linux-uclibc --disable-locale --disable-ipv6
+RSYNC_CONFIGURE_OPTS=--host=$(FLAT_COMPILE) --disable-locale --disable-ipv6
 
 $(DL_DIR)/$(RSYNC_SOURCE):
 	$(WGET) -P $(DL_DIR) $(RSYNC_SITE)/$(RSYNC_SOURCE)
@@ -21,17 +21,18 @@ $(RSYNC_DIR)/.unpacked: $(DL_DIR)/$(RSYNC_SOURCE)
 	touch $(RSYNC_DIR)/.unpacked
 
 $(RSYNC_DIR)/.configured: $(RSYNC_DIR)/.unpacked
-	chmod a+x $(RSYNC_DIR)/configure
-	$(PATCH_KERNEL) $(RSYNC_DIR) package/rsync rsync.patch
-	cd $(RSYNC_DIR); ./configure $(RSYNC_CONFIGURE_OPTS)
+	$(PATCH_KERNEL) $(RSYNC_DIR) $(RSYNC_DIR)/patches threaded-receiver.diff
+	$(PATCH_KERNEL) $(RSYNC_DIR) package/rsync rsync-thread_retval.diff
+	$(PATCH_KERNEL) $(RSYNC_DIR) package/rsync rsync-thread_stack_256k.diff
+	$(PATCH_KERNEL) $(RSYNC_DIR) package/rsync rsync-unsupported.diff
+	$(PATCH_KERNEL) $(RSYNC_DIR) package/rsync rsync-gnuconfig.diff
+	cd $(RSYNC_DIR); LDFLAGS="-Wl,-elf2flt=-s262144" ./configure $(RSYNC_CONFIGURE_OPTS)
 	touch $(RSYNC_DIR)/.configured
 	mkdir -p $(TARGET_DIR)/bin
 
-
 rsync: $(RSYNC_DIR)/.configured
-	make -C $(RSYNC_DIR)/ STAGEDIR=$(STAGING_DIR)
+	make -C $(RSYNC_DIR)/ rsync
 	cp -f $(RSYNC_DIR)/rsync $(TARGET_DIR)/bin/rsync
-	$(TARGET_STRIP) $(TARGET_DIR)/bin/rsync
 
 rsync-dirclean:
 	rm -rf $(RSYNC_DIR)
