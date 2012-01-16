9b9c4717456129306001786e14e4791b7cdfebca
Dimitar Penev <dpn@switchvoice.com>
2012-01-16 14:01:27 +0000

    Bump to Inadyn 1.98.1
    git-svn-id: https://switchfin.svn.sourceforge.net/svnroot/switchfin/switchfin/trunk@674 705e1490-8ef5-445a-a6c1-187d55b79fae

--- package/inadyn/inadyn.mk
+++ package/inadyn/inadyn.mk
@@ -4,8 +4,8 @@
 #
 # Copyright @ 2010 SwitchFin <dpn@switchfin.org>
 #########################################################
-INADYN_SITE=ftp://ftp.vmlinux.org/pub/People/jocke/inadyn/
-INADYN_VERSION=1.97.4
+INADYN_SITE=http://cloud.github.com/downloads/troglobit/inadyn
+INADYN_VERSION=1.98.1
 INADYN_SOURCE=inadyn-$(INADYN_VERSION).tar.bz2
 INADYN_CROSS_COMPILE_PATCH=inadyn.patch
 INADYN_DIR_BASENAME=inadyn
@@ -13,7 +13,7 @@ INADYN_DIR=$(BUILD_DIR)/$(INADYN_DIR_BASENAME)-$(INADYN_VERSION)
 
 
 $(DL_DIR)/$(INADYN_SOURCE):
-	$(WGET) -P $(DL_DIR) $(INADYN_SITE)/$(INADYN_SOURCE)
+	$(WGET)  -P $(DL_DIR) $(INADYN_SITE)/$(INADYN_SOURCE)
 
 $(INADYN_DIR)/.unpacked: $(DL_DIR)/$(INADYN_SOURCE)
 	tar -xf $(DL_DIR)/$(INADYN_SOURCE) -C $(BUILD_DIR)
